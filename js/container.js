5fcb1f990493fc1ea961da6a0b954ae5683b3103
Agata Jankowski <mail@agata.de>
2019-05-22 21:18:55 +0200

    Add DMAP free allocation

--- js/container.js
+++ js/container.js
@@ -12,32 +12,31 @@ let data = {
         {
             "name": "Superblock", "size": 1
         }, {
-            "name": "DMAP", "size": 16
+            "name": "DMAP",
         }, {
-            "name": "FAT", "size": 256
+            "name": "FAT", "size": 128
         }, {
             "name": "Root", "size": 64
         }, {
-            "name": "Data", "size": 0
+            "name": "Data", "size": 64
         }]
 };
 
 // Forcing object copy by value not reference
-const newData = JSON.parse(JSON.stringify(data));
+var newData = JSON.parse(JSON.stringify(data));
 
 /*
 * DMAP
 */
 
-// Marking free space in DMAP als "F"
-// Size value is (16 available dmap blocks / 65087 data blocks)
-/*for (let i = 0; i < 65086; i++) {
-    newData.children[1].children[i] = [{"name": "F", "size": 0.00024582481}];
-}*/
+// Allocating free space in DMAP als "F"
 
+newData.children[1].children = [{"name": "F", "size": 1}];
 
+for (let i = 1; i < 16; i++) {
+    newData.children[1].children[i] = {"name": "F", "size": 1};
+}
 
-// newData.children[1].children = [{"name": "A", "size": 2}];
 
 /*
 * ROOT DIRECTORY
@@ -45,25 +44,3 @@ const newData = JSON.parse(JSON.stringify(data));
 
 // Adding new file to root directory
 newData.children[3].children = [{"name": "File 1", "size": 1}];
-
-// JSON data
-/*var data = {
-    "name": "Container",
-    "children": [
-    {
-        "name": "Superblock",
-        "children": [{"name": "Sub A1", "size": 1}]
-    }, {
-        "name": "DMAP",
-        "children": [{"name": "Sub B1", "size": 16}]
-    }, {
-        "name": "FAT",
-        "children": [{"name": "Sub A1", "size": 256}]
-    }, {
-        "name": "Root",
-        "children": [{"name": "Sub A1", "size": 64}]
-    }, {
-        "name": "Data",
-        "children": [{"name": "Sub A1", "size": 0}]
-    }]
-};*/
