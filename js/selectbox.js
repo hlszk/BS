f5214cba1a00a9c8f4b0fe52d11dada8ff22b751
Agata Jankowski <mail@agata.de>
2019-05-23 12:24:36 +0200

    Add comments to selection box code

--- js/selectbox.js
+++ js/selectbox.js
@@ -1,14 +1,22 @@
-var fileOptions = [
+/*
+* Selection box for adding or removing files in container
+*/
+
+// Options of selection box
+const fileOptions = [
     "Select one:",
     "Add small file (one data block)",
     "Add big file (two data blocks)",
     "Remove file"
 ];
 
+// Add selection menu element to DOM
 const select = d3.select('body')
     .append('select')
+    // Selection handler on changing options
     .on('change', selectOption);
 
+// Add selection options to newly created DOM element
 const options = select
     .selectAll('option')
     .data(fileOptions).enter()
@@ -17,9 +25,11 @@ const options = select
     .attr('disabled', d => (d === fileOptions[0]) ? true : null )
     .text(d => d);
 
+// Selection handler
 function selectOption() {
     let selectValue = d3.select('select').property('value');
-    
+
+    // Calling FUSE methods
     switch (selectValue) {
         case fileOptions[0]: break;
         case fileOptions[1]: fuseOpen(false); break;
