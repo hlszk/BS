510be8d202ff5461ccf434ad13e634c45b62bf04
Agata Jankowski <mail@agata.de>
2019-05-23 12:19:50 +0200

    Add disable of first selection option

--- js/selectbox.js
+++ js/selectbox.js
@@ -1,4 +1,5 @@
 var fileOptions = [
+    "Select one:",
     "Add small file (one data block)",
     "Add big file (two data blocks)",
     "Remove file"
@@ -12,14 +13,17 @@ const options = select
     .selectAll('option')
     .data(fileOptions).enter()
     .append('option')
+    // Disable first selection option
+    .attr('disabled', d => (d === fileOptions[0]) ? true : null )
     .text(d => d);
 
 function selectOption() {
     let selectValue = d3.select('select').property('value');
     
     switch (selectValue) {
-        case fileOptions[0]: fuseOpen(false); break;
-        case fileOptions[1]: fuseOpen(true); break;
+        case fileOptions[0]: break;
+        case fileOptions[1]: fuseOpen(false); break;
+        case fileOptions[2]: fuseOpen(true); break;
     }
     
     updateData();
